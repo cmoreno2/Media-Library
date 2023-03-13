6c2b769e01f9dd699389c8be7cb86dd8c93682b2
Cesar Moreno <cmoreno2@my.wctc.edu>
2023-03-13 13:01:37 -0500

    [1.04] Movie class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+Movie movie = new Movie
+{
+    mediaId = 123,
+    title = "Greatest Movie Ever, The (2023)",
+    genres = { "Comedy", "Romance" }
+};
+
+Console.WriteLine(movie.Display());
+
+logger.Info("Program ended");
