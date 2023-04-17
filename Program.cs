1dd997eee32c9099a3790dbada23b7b62e40bfab
Cesar Moreno <cmoreno2@my.wctc.edu>
2023-04-16 23:35:01 -0500

    Updated Program Now allows runtime and director

--- Program.cs
+++ Program.cs
@@ -5,10 +5,75 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.csv";
+
 logger.Info("Program started");
 
-string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-logger.Info(scrubbedFile);
-MovieFile movieFile = new MovieFile(scrubbedFile);
+MovieFile movieFile = new MovieFile(movieFilePath);
+
+string choice = "";
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Add Movie");
+  Console.WriteLine("2) Display All Movies");
+  Console.WriteLine("Enter to quit");
+  // input selection
+  choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+  
+  if (choice == "1")
+  {
+    // Add movie
+        Movie movie = new Movie();
+    // ask user to input movie title
+    Console.WriteLine("Enter movie title");
+    // input title
+    movie.title = Console.ReadLine();
+    // verify title is unique
+    if (movieFile.isUniqueTitle(movie.title)){
+    // input genres
+      string input;
+      do
+      {
+        // ask user to enter genre
+        Console.WriteLine("Enter genre (or done to quit)");
+        // input genre
+        input = Console.ReadLine();
+        // if user enters "done"
+        // or does not enter a genre do not add it to list
+        if (input != "done" && input.Length > 0)
+        {
+          movie.genres.Add(input);
+        }
+      } while (input != "done");
+      // specify if no genres are entered
+      if (movie.genres.Count == 0)
+      {
+        movie.genres.Add("(no genres listed)");
+      }
+
+    // asking for director
+    Console.WriteLine("Who was the dirctor of this movie?");
+    // director input
+    movie.director = Console.ReadLine();
+
+    // asking for runtime
+    Console.WriteLine("What was the total runtime of the movie?(hh:mm:ss)");
+    // director input
+    movie.runningTime = TimeSpan.Parse(Console.ReadLine());
+            // add movie
+      movieFile.AddMovie(movie);
+    }
+    
+  } else if (choice == "2")
+  {
+    // Display All Movies
+    foreach(Movie m in movieFile.Movies)
+    {
+      Console.WriteLine(m.Display());
+    }
+  }
+} while (choice == "1" || choice == "2");
 
 logger.Info("Program ended");
